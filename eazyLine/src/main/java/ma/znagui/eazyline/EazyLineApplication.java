6a977abb46483accb5409625925dcf6212deb88f
znagui <znagui.info@gmail.com>
2024-11-12 12:08:27 +0100

    Configuration YML

---
+++ eazyLine/src/main/java/ma/znagui/eazyline/EazyLineApplication.java
@@ -0,0 +1,13 @@
+package ma.znagui.eazyline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazyLineApplication.class, args);
+    }
+
+}
